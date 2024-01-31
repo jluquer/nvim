c87d79f8a48ad402aad192c52e22208d25e52392
Javier Luque <javier.lr9@gmail.com>
2024-01-31 12:40:37 +0100

    created indent.lua and replaced general with others.lua

---
+++ lua/plugins/others.lua
@@ -0,0 +1,10 @@
+return {
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  { 'folke/which-key.nvim',  opts = {} },
+
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+}
