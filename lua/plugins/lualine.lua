b3184925a45b4119d66c3b4930e804783c8f4a7f
Javier Luque <javier.lr9@gmail.com>
2024-02-02 13:09:38 +0100

    lualine icons and rounded

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,14 @@
 local constants = require('config.constants')
 
 return {
-  -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
   -- See `:help lualine.txt`
   opts = {
     options = {
-      icons_enabled = false,
+      icons_enabled = true,
       theme = constants.colorscheme,
-      component_separators = '|',
-      section_separators = '',
+      section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' },
     },
   },
 }
