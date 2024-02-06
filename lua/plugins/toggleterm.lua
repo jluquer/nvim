73c4d285c1132966cc8bb9b258fb33e3dab96f4c
Javier Luque Rodríguez <javier.luque@stlexternals.com>
2024-02-06 12:06:45 +0100

    changed toggleterm keymaps

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,8 +1,8 @@
-local shell = require('utils').is_windows() and "pwsh.exe" or vim.o.shell
+local shell = require("utils").is_windows() and "pwsh.exe" or vim.o.shell
 
 return {
-  'akinsho/toggleterm.nvim',
-  event = 'VeryLazy',
+  "akinsho/toggleterm.nvim",
+  event = "VeryLazy",
 
   opts = {
     size = 10,
@@ -19,27 +19,27 @@ return {
   },
 
   keys = function()
-    local toggleterm = require('toggleterm')
+    local toggleterm = require "toggleterm"
 
     local function float_term()
       toggleterm.toggle(1)
     end
 
     local function horizontal_term()
-      toggleterm.toggle(2, nil, nil, 'horizontal')
+      toggleterm.toggle(2, nil, nil, "horizontal")
     end
 
     local function horizontal_right_term()
-      toggleterm.toggle(3, nil, nil, 'horizontal')
+      toggleterm.toggle(3, nil, nil, "horizontal")
     end
 
     return {
-      { '<leader>tt', float_term,            mode = { "n" },           desc = 'Toggle horizontal terminal' },
-      { '<A-t>',      float_term,            mode = { "n", "i", 't' }, desc = 'Toggle floating terminal' },
-      { '<A-i>',      horizontal_term,       mode = { "n", "i", 't' }, desc = 'Toggle horizontal terminal' },
-      { '<A-r>',      horizontal_right_term, mode = { "n", "i", 't' }, desc = 'Toggle vertical terminal' },
-      { '<Esc><Esc>', '<C-\\><C-n>',         mode = 't' },
-      { 'kkjj',       '<C-\\><C-n>',         mode = 't' },
+      { "<leader>tt", float_term,            mode = { "n" },           desc = "Toggle horizontal terminal" },
+      { "<A-i>",      float_term,            mode = { "n", "i", "t" }, desc = "Toggle floating terminal" },
+      { "<A-l>",      horizontal_term,       mode = { "n", "i", "t" }, desc = "Toggle horizontal terminal" },
+      { "<A-r>",      horizontal_right_term, mode = { "n", "i", "t" }, desc = "Toggle vertical terminal" },
+      { "<Esc><Esc>", "<C-\\><C-n>",         mode = "t" },
+      { "kkk",        "<C-\\><C-n>",         mode = "t" },
     }
   end,
 
