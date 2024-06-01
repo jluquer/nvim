21a73addc5790207eee8c77b824cc8c7b8bf4503
Javier Luque <javier.lr9@gmail.com>
2024-06-01 14:28:08 +0200

    go.nvim failing because of inlay_hint bug in their plugin

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,17 +1,18 @@
-return {
-  "ray-x/go.nvim",
-  dependencies = { -- optional packages
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    require("go").setup {
-      tag_transform = "camelcase",
-      tag_options = "",
-    }
-  end,
-  event = { "CmdlineEnter" },
-  ft = { "go", "gomod" },
-  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-}
+return {}
+-- return {
+--   "ray-x/go.nvim",
+--   dependencies = { -- optional packages
+--     "ray-x/guihua.lua",
+--     "neovim/nvim-lspconfig",
+--     "nvim-treesitter/nvim-treesitter",
+--   },
+--   config = function()
+--     require("go").setup {
+--       tag_transform = "camelcase",
+--       tag_options = "",
+--     }
+--   end,
+--   event = { "CmdlineEnter" },
+--   ft = { "go", "gomod" },
+--   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+-- }
