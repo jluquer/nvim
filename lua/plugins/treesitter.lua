ba772275ff42e13be6e1f2a8031da43b5e0ac3e1
Javier Luque <javier.lr9@gmail.com>
2024-06-03 14:24:18 +0200

    fix lsp hover failing because of missing treesitter markdown

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -24,6 +24,7 @@ return {
           "vim",
           "bash",
           "astro",
+          "markdown",
         },
 
         -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
